6113e0f647aaddff55570af04bf840df479d97fb
lujunhui <385935631@qq.com>
2019-08-31 18:01:43 +0800

    测试eslintignore

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="cart">
-     我是购物车
+    <van-row class="news">
+      <van-row class="consignee">
+        <van-col span="6">收货人</van-col>
+        <van-col span="18" class="tl">陆俊慧先生</van-col>
+      </van-row>
+      <van-row class="tel">
+        <van-col span="6">电话</van-col>
+        <van-col span="18" class="tl">188********</van-col>
+      </van-row>
+      <van-row class="address">
+        <van-col span="6">地址</van-col>
+        <van-col span="18" class="tl">上海市浦东新区航都路18号--黑马程序员</van-col>
+      </van-row>
+    </van-row>
   </div>
 </template>
 
@@ -8,5 +21,11 @@
 .cart {
   // 头部和底部都是固定定位 所以要留出50像素
   margin: 50px 0;
+  .news {
+    padding:20px 50px;
   }
+  .tel{
+    margin:20px 0;
+  }
+}
 </style>
