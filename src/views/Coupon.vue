576a7f586a2a20d16d500be67ccb10e58be54298
lujunhui <385935631@qq.com>
2019-09-02 17:24:55 +0800

    修改bug完成

--- src/views/Coupon.vue
+++ src/views/Coupon.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class='coupon'>
+  <div class="coupon">
     <!-- 优惠券单元格 -->
-    <van-coupon-cell :coupons='coupons' :chosen-coupon='chosenCoupon' @click='showList = true'  />
+    <van-coupon-cell :coupons="coupons" :chosen-coupon="chosenCoupon" @click="showList = true" />
 
     <!-- 优惠券列表 -->
-    <van-popup v-model='showList' position='bottom'>
+    <van-popup v-model="showList" position="bottom">
       <van-coupon-list
-        :coupons='coupons'
-        :chosen-coupon='chosenCoupon'
-        @change='onChange'
-        :show-exchange-bar=false
+        :coupons="coupons"
+        :chosen-coupon="chosenCoupon"
+        @change="onChange"
+        :show-exchange-bar="false"
       />
     </van-popup>
   </div>
@@ -19,7 +19,6 @@
 const coupon1 = {
   available: 1,
   condition: '满30元\n优惠1元',
-  reason: '',
   value: 100,
   name: '1元优惠券',
   startAt: 1567204000,
@@ -31,7 +30,6 @@ const coupon1 = {
 const coupon2 = {
   available: 2,
   condition: '满30元\n优惠2元',
-  reason: '',
   value: 200,
   name: '2元优惠券',
   startAt: 1567204000,
@@ -43,7 +41,6 @@ const coupon2 = {
 const coupon3 = {
   available: 3,
   condition: '满30元\n优惠3元',
-  reason: '',
   value: 300,
   name: '3元优惠券',
   startAt: 1567204000,
@@ -65,9 +62,13 @@ export default {
     onChange (index) {
       this.showList = false
       this.chosenCoupon = index
+      // 如果不是-1就说明使用了优惠券
       if (index !== -1) {
         this.$store.state.couponmoney = this.coupons[index].value / 100
         console.log(this.$store.state.couponmoney)
+      } else {
+        // 如果是-1就说明没用优惠券 vuex优惠券价格置为0
+        this.$store.state.couponmoney = 0
       }
       this.$router.push('/home/cart')
     }
@@ -76,10 +77,10 @@ export default {
 </script>
 <style lang='less' scoped>
 // 修改vant-ui默认的样式
-/deep/ .van-overlay{
-  display:none
+/deep/ .van-overlay {
+  display: none;
 }
-/deep/ .van-coupon-list{
-   height:80%;
+/deep/ .van-coupon-list {
+  height: 80%;
 }
 </style>
