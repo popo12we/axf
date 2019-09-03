ab4259698be5c3bb127d9b6ac31f338c35461612
lujunhui <385935631@qq.com>
2019-09-03 11:35:19 +0800

    究极版1

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,62 +1,65 @@
 <template>
-  <div class='list'>
+  <div class="list">
+
+    <!-- 通知栏 -->
+    <van-notice-bar
+      mode="closeable"
+      text="好消息，好消息，特大好消息，老陆超市满30最多可享受3元优惠!!~~"
+      left-icon="volume-o"
+    />
+
     <!-- 左侧导航条 -->
-<van-notice-bar
-  mode="closeable"
-  text="好消息，好消息，特大好消息，老陆超市满30最多可享受3元优惠!!~~"
-  left-icon="volume-o"
-/>
-    <div class='leftList'>
-      <van-sidebar v-model='activeKey'>
+    <div class="leftList">
+      <van-sidebar v-model="activeKey">
         <van-sidebar-item
-          :title='item.name'
-          v-for='item in menuList'
-          @click='changeProductList(item.id)'
-          :key='item.id'
+          :title="item.name"
+          v-for="item in menuList"
+          @click="changeProductList(item.id)"
+          :key="item.id"
         />
       </van-sidebar>
     </div>
 
     <!-- 右侧商品列表 -->
-    <div class='right-nav'>
-      <ul class='right-nav-ul'>
-        <li class='right-nav-li' v-for='item in productsList' :key='item.id'>
+    <div class="right-nav">
+      <ul class="right-nav-ul">
+        <li class="right-nav-li" v-for="item in productsList" :key="item.id">
           <!-- 图片 -->
-          <div class='pic'>
-            <img :src='item.img' />
+          <div class="pic">
+            <img :src="item.img" />
           </div>
 
           <!-- 商品信息 -->
-          <div span='20' class='product-info'>
-            <p class='productname'>{{item.name}}</p>
-            <p class='specifics'>{{item.specifics}}</p>
+          <div span="20" class="product-info">
+            <p class="productname">{{item.name}}</p>
+            <p class="specifics">{{item.specifics}}</p>
             <p>
-              <b class='market_price'>￥{{item.market_price}}</b>
-              <s class='price'>￥{{item.price}}</s>
+              <b class="market_price">￥{{item.market_price}}</b>
+              <s class="price">￥{{item.price}}</s>
             </p>
             <!-- 步进器 -->
             <!-- min的意思是代表最小值是0 不写的话默认是1 -->
             <!-- count:(item._had_pm-0+1)说明 减0是为了转成数字类型 步进器默认的值还是从1开始算的，所以这样写的目的是为了同步步进器的值-->
             <van-stepper
-              v-model='item._had_pm'
-              min='0'
-              class='step'
-              @plus='addbeeCart({
+              v-model="item._had_pm"
+              min="0"
+              class="step"
+              @plus="addbeeCart({
                id:item.id,
                count:(item._had_pm-0+1),
                name:item.name,
                img:item.img,
                price:item.market_price-0,
                isSelected:true
-               })'
-              @minus='minusbeeCart({
+               })"
+              @minus="minusbeeCart({
                id:item.id,
                count:(item._had_pm-0+1),
                name:item.name,
                img:item.img,
                price:item.market_price-0,
                isSelected:true
-               })'
+               })"
             />
           </div>
         </li>
